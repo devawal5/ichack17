82736a247bd8c626d7bbfae4b2b2d0008f736229
James <jysdoran@gmail.com>
2017-02-04 18:37:25 +0000

    Removed some dumb stuff

--- SerialReader/src/Reader.java
+++ SerialReader/src/Reader.java
@@ -6,23 +6,23 @@ public class Reader {
 
     public static void main(String[] args) {
         SerialPort serialPort = new SerialPort("/dev/ttyACM1");
-        SerialPort serialPort2 = new SerialPort("/dev/ttyACM2");
+        //SerialPort serialPort2 = new SerialPort("/dev/ttyACM2");
         try {
    //         serialPort.closePort();//Close serial port
             serialPort.openPort();//Open serial port
             serialPort.setParams(9600, 8, 1, 0);//Set params.
-            serialPort2.openPort();//Open serial port
-            serialPort2.setParams(9600, 8, 1, 0);//Set params.
+            //serialPort2.openPort();//Open serial port
+            //serialPort2.setParams(9600, 8, 1, 0);//Set params.
 
             //byte[] buffer = serialPort.readBytes(10);//Read 10 bytes from serial port
             String bstring;
             while (true) {
-                bstring = serialPort.readString(50);
+                bstring = serialPort.readString(60);
                 String[] splitted = bstring.split("_");
                 System.out.println("A: "+ splitted[1]);
-                bstring = serialPort2.readString(50);
-                splitted = bstring.split("_");
-                System.out.println("B: "+ splitted[1]);
+                //bstring = serialPort2.readString(50);
+                //splitted = bstring.split("_");
+                //System.out.println("B: "+ splitted[1]);
             }
 //            serialPort.closePort();//Close serial port
             /*for (byte b :  buffer) {
